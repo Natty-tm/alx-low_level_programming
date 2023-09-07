c6f578d9deeaad624b66cd42a5fabae0ef84f46f
Natty-tm <natty.tm@yahoo.com>
2023-09-07 14:22:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - this is the main function
+ * return: always zero
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
