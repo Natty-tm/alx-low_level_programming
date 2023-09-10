cd1617cfe93d9ab889e3a9bdaa10d203c08a61f2
Natty-tm <natty.tm@yahoo.com>
2023-09-10 11:20:41 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function
+ * Return: is always zero
+ */
+int main(void)
+{
+	int s;
+
+	for (s = '0'; s < 10; s++)
+		printf("%d", s);
+	printf("\n");
+	return (0);
+}
