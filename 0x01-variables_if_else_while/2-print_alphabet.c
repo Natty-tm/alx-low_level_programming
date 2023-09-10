f29d417bfa9285c13cba1d5484609d594840dbd7
Natty-tm <natty.tm@yahoo.com>
2023-09-10 10:03:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this the main function
+ * Return: is alwas zero
+ */
+
+int main(void)
+{
+	char p;
+
+	for (p = 'a'; p <= 'z'; p++)
+	putchar(p);
+	putchar('\n');
+	return (0);
+}
