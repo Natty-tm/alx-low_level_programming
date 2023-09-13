d7572f2aaedb4be6b93a6466d2f5cc6a095c29ae
Natty-tm <natty.tm@yahoo.com>
2023-09-13 08:34:46 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - is the main finction
+ * Return: is always zreo
+ */
+
+void print_alphabet(void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
