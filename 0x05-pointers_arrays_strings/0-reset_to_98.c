09eb252b560ec731c6ed53d0462ee6c2129652d5
Natty-tm <natty.tm@yahoo.com>
2023-09-20 10:12:46 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer and updates the value
+ * @n: an intiger input pointer
+ * Return: is always zero
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
